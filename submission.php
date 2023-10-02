695eb4189a89b5eaefbbd67930a8fb5afa02c1b7
raywongkaren <s22110343@student.unklab.ac.id>
2023-10-02 09:58:51 +0800

    commit pertama saya

---
+++ submission.php
@@ -0,0 +1,9 @@
+<?php
+    // POST Data
+    $data['name'] = $_POST['firstName'] . " " . $_POST['lastName'];
+    $data['email'] = $_POST['email'];
+    $data['message'] = $_POST['message'];
+
+    echo json_decode($data);
+    exit;
+?>
---
+++ web-react-kedua/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h2>Selamat datang di class Front-End Web.</h2>
+
+      <h2>Student Profile</h2>
+      <table>
+        <tr>
+          <td>Nama</td>
+          <td>Wongkaren, Ray Mcdylan</td>
+        </tr>
+        <tr>
+          <td>Fakultas</td>
+          <td>Fakultas Ilmu Komputer</td>
+        </tr>
+        <tr>
+          <td>Jurusan</td>
+          <td>Informatika</td>
+        </tr>
+        <tr>
+          <td>Alamat</td>
+          <td>Manado, Malalayang</td>
+        </tr>
+        <tr>
+          <td>Nim</td>
+          <td>105022110062</td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td>s22110343@student.unklab.ac.id</td>
+        </tr>
+      </table>
+      
+    </div>
+  );
+}
+
+export default App;
