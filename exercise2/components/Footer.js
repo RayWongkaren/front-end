8a20ee47f18687452f3b22e979ae3ed8ca92afa3
raywongkaren <s22110343@student.unklab.ac.id>
2023-10-23 19:41:51 +0800

    Lecture 19

---
+++ exercise2/components/Footer.js
@@ -0,0 +1,10 @@
+class FooterPage extends React.Component {
+    render() {
+        return(
+            <div class="footer">
+                <h2>Universitas Klabat</h2>
+                <div>Jl. Arnold Mononutu, Airmadidi Bawah, Kec. Airmadidi, Kabupaten Minahasa Utara, Sulawesi Utara 95371</div>
+            </div>
+        );
+    }
+}
